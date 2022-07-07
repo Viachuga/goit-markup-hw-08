4a8b4b27a6cc87693ca5c8ea6869e41c3d083126
Viachuga <103814122+Viachuga@users.noreply.github.com>
2022-07-07 21:49:41 +0300

    fixed paddings ets

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,12 +3,14 @@
       mobileMenuOpenBtn: document.querySelector('[data-menu-open]'),
       mobileMenuCloseBtn: document.querySelector('[data-menu-close]'),
       menu: document.querySelector('[data-menu]'),
+      body: document.querySelector('body'),
     };
   
     refs.mobileMenuOpenBtn.addEventListener('click', toggleMenu);
     refs.mobileMenuCloseBtn.addEventListener('click', toggleMenu);
   
     function toggleMenu() {
-      refs.menu.classList.toggle("is-hidden")
+      refs.menu.classList.toggle("is-hidden");
+      refs.body.classList.toggle("no-scroll");
     }
   })();
